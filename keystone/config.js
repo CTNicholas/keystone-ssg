82645a97bfc5eef0998e806a6dd6702872e48708
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2020-07-03 13:13:54 +0100

    rename complete

--- keystone/config.js
+++ keystone/config.js
@@ -1,7 +1,7 @@
-const defaultConfig = require('./statis.default.config.js')
+const defaultConfig = require('./keystone.default.config.js')
 
 const path = require('path')
-const customConfig = require(path.resolve(process.cwd(), 'statis.config.js'))
+const customConfig = require(path.resolve(process.cwd(), 'keystone.config.js'))
 
 module.exports = {
   ...defaultConfig,
